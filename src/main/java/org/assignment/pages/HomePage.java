22bec37db214b50e9c7a26f6d35db5b55c0e073c
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-04-25 10:00:52 +0530

    1. HomePage added

---
+++ src/main/java/org/assignment/pages/HomePage.java
@@ -0,0 +1,29 @@
+package org.assignment.pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By electronicsTab = By.xpath("//li[contains(@class,'vl-flyout-nav__js-tab')]//a[text()='Electronics']"
+    );
+
+    public HomePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public void openHome(String url) {
+        driver.get(url);
+        wait.until(ExpectedConditions.urlContains("ebay.com"));
+    }
+
+    public void clickElectronicsTab() {
+        wait.until(ExpectedConditions.elementToBeClickable(electronicsTab));
+        driver.findElement(electronicsTab).click();
+    }
+
+}

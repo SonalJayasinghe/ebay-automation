03958eab824d9ec22674b0936ad49cc932c688fa
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-04-25 09:49:51 +0530

    1. InitTest setup teardown methdods added

--- src/test/java/tests/InitTest.java
+++ src/test/java/tests/InitTest.java
@@ -1,5 +1,29 @@
 package tests;
 
-public class InitTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+import java.time.Duration;
 
+
+public class InitTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp(){
+        //file path is in structure for macos
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/Drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public void tearDown(){
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }

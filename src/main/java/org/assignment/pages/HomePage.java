ea7ac4e7236b5a7f4cc278a2fc500aea6991609d
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-04-25 10:37:23 +0530

    1. geToCellPhones method added 2. test runner function updated and validated

--- src/main/java/org/assignment/pages/HomePage.java
+++ src/main/java/org/assignment/pages/HomePage.java
@@ -1,6 +1,7 @@
 package org.assignment.pages;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -8,8 +9,12 @@ public class HomePage {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    private By electronicsTab = By.xpath("//li[contains(@class,'vl-flyout-nav__js-tab')]//a[text()='Electronics']"
-    );
+    private By electronicsTab = By.xpath("//li[contains(@class,'vl-flyout-nav__js-tab')]//a[text()='Electronics']");
+    //private By cellPhonesLink = By.linkText("Cell Phones & Smartphones"); - not available on the page, date: friday 25, april 2025
+    private By cellPhonesLink = By.linkText("Smartphones and accessories");
+
+
+
 
     public HomePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -21,9 +26,10 @@ public class HomePage {
         wait.until(ExpectedConditions.urlContains("ebay.com"));
     }
 
-    public void clickElectronicsTab() {
-        wait.until(ExpectedConditions.elementToBeClickable(electronicsTab));
-        driver.findElement(electronicsTab).click();
+    public void goToCellPhones() {
+        WebElement menu = wait.until(ExpectedConditions.visibilityOfElementLocated(electronicsTab));
+        new Actions(driver).moveToElement(menu).perform();
+        wait.until(ExpectedConditions.elementToBeClickable(cellPhonesLink)).click();
     }
 
 }
--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -10,6 +10,6 @@ public class TestRunner extends InitTest{
         String url = "https://www.ebay.com/";
         HomePage homePage = new HomePage(driver, wait);
         homePage.openHome(url);
-        homePage.clickElectronicsTab();
+        homePage.goToCellPhones();
     }
 }

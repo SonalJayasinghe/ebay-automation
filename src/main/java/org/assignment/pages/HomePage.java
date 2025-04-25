998a9a8448abacaf50ac7c1e07c9fa59aaf00a42
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-04-25 11:49:30 +0530

    1. homepage updated

--- src/main/java/org/assignment/pages/HomePage.java
+++ src/main/java/org/assignment/pages/HomePage.java
@@ -1,7 +1,6 @@
 package org.assignment.pages;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,11 +9,6 @@ public class HomePage {
     private WebDriverWait wait;
 
     private By electronicsTab = By.xpath("//li[contains(@class,'vl-flyout-nav__js-tab')]//a[text()='Electronics']");
-    //private By cellPhonesLink = By.linkText("Cell Phones & Smartphones"); - not available on the page, date: friday 25, april 2025
-    private By cellPhonesLink = By.linkText("Smartphones and accessories");
-
-
-
 
     public HomePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -26,10 +20,8 @@ public class HomePage {
         wait.until(ExpectedConditions.urlContains("ebay.com"));
     }
 
-    public void goToCellPhones() {
-        WebElement menu = wait.until(ExpectedConditions.visibilityOfElementLocated(electronicsTab));
-        new Actions(driver).moveToElement(menu).perform();
-        wait.until(ExpectedConditions.elementToBeClickable(cellPhonesLink)).click();
+    public void goToElectronicsPage() {
+        wait.until(ExpectedConditions.elementToBeClickable(electronicsTab)).click();
     }
 
 }
